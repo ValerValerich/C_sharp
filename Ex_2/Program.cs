5432e2a378e4385257dc4d4ff1d589cfe2706eee
ValerValerich <zvalera1991@mail.ru>
2022-10-17 01:57:11 +0300

    ex_2 ok

--- Ex_2/Program.cs
+++ Ex_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите первое число:");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+    Console.WriteLine($"Число {a} меньше, чем {b}");
+}
+else if (a>b)
+{
+    Console.WriteLine($"Число {b} меньше, чем {a}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
