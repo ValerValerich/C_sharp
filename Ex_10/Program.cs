7238f273654225520aa7d8f060de7f8066e83b1c
ValerValerich <zvalera1991@mail.ru>
2022-10-18 23:39:30 +0300

    Ex_13 OK

--- Ex_10/Program.cs
+++ Ex_10/Program.cs
@@ -4,6 +4,6 @@ Console.Clear();
 Console.WriteLine("Введите трехзначное число:");
 int num = int.Parse(Console.ReadLine()!);
 
-int result = num/10%10;
+int result = num / 10 % 10;
 
 Console.WriteLine($"В числе {num} вторая цифра - это {result}");
---
+++ Ex_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int num = new Random().Next(1, 100000000);
+int result = num;
+while (result > 1000)
+{
+    result /= 10;
+}
+if (result > 99)
+{
+    result = result % 10;
+    Console.WriteLine($"В числе {num} третья цифра {result}");
+}
+else
+{
+    Console.WriteLine($"В числе {num} третьей цифры нет");
+}
