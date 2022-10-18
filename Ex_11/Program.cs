9d68cb2aad868d5590f010d9feec38270cad36f9
ValerValerich <zvalera1991@mail.ru>
2022-10-18 22:07:12 +0300

    Ex_11 Ok

---
+++ Ex_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+
+int first = num / 100 * 10;
+int second = num % 10;
+
+Console.Write($"Число {num} превращаем в {first+second}");
