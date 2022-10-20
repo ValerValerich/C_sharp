45ca1a63d9fc885c101128e27a843ee2ee258af3
ValerValerich <zvalera1991@mail.ru>
2022-10-20 23:28:48 +0300

    Ex_23 OK

---
+++ Ex_23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+string GetThirdLevel(int n)
+{
+    string answer ="";
+    for (int i = 1; i <= n; i++)
+    {
+        answer =answer + ((Math.Pow(i, 3)).ToString() + ", ");
+    }
+    return answer;
+}
+Console.WriteLine("Введите число");
+Console.WriteLine($"{GetThirdLevel(int.Parse(Console.ReadLine()!)).TrimEnd(' ').TrimEnd(',')}");
