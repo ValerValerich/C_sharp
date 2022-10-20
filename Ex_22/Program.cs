2f67ea7e2992e7a447e23086368d1d43f087b5a9
ValerValerich <zvalera1991@mail.ru>
2022-10-20 22:32:25 +0300

    Ex_22 OK

--- Ex_22/Program.cs
+++ Ex_22/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+
+string GetSQRT(int n)
+{
+    string answer ="";
+    for (int i = 1; i <= n; i++)
+    {
+        answer =answer + ((i*i).ToString() + ", ");
+    }
+    return answer;
+}
+Console.WriteLine("Введите число");
+Console.WriteLine($"{GetSQRT(int.Parse(Console.ReadLine()!)).TrimEnd(' ').TrimEnd(',')}");
