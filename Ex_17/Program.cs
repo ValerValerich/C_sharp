1c5bdfcda08e6e426695fdb27f3a7def4444a604
ValerValerich <zvalera1991@mail.ru>
2022-10-20 20:21:21 +0300

    Ex_17 OK

---
+++ Ex_17/Program.cs
@@ -0,0 +1,36 @@
+﻿// Принимает на вход координаты точки (X и Y) и выдает номер координатной четверти
+
+Console.Clear();
+
+int findQuater(int x, int y)
+{
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 4;
+        }
+    }
+    else
+    {
+        if (y > 0)
+        {
+            return 2;
+        }
+        else
+        {
+            return 3;
+        }
+    }
+}
+
+int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+
+int quater = findQuater(X, Y);
+
+Console.WriteLine($"{quater}");
