f86689af0a34143b8741001dfe31e79566a58440
ValerValerich <zvalera1991@mail.ru>
2022-10-20 21:00:39 +0300

    Ex_18 Ok

---
+++ Ex_18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (х и у)
+
+string getList(int x)
+{
+    if (x == 1)
+    {
+        return "х > 0, y > 0";
+    }
+    else if (x == 2)
+    {
+        return "х < 0, y > 0";
+    }
+    else if (x == 3)
+    {
+        return "х < 0, y < 0";
+    }
+    else if (x == 4)
+    {
+        return "х > 0, y < 0";
+    }
+    else{
+        return "Нет такой четверти";
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(getList(n));
