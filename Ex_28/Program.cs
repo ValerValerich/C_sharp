19373a552a2bd90fd73b4c802a19c350caf8df86
ValerValerich <zvalera1991@mail.ru>
2022-11-09 22:32:07 +0300

    Ex 28 Ok

---
+++ Ex_28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Clear();
+
+int GetMult(int x)
+{
+
+    if (x > 0)
+    {
+        int mult = 1;
+        for (int i = 1; i <= x; i++)
+        {
+            mult *= i;
+        }
+        return mult;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+Console.WriteLine("Введите число");
+Console.WriteLine(GetMult(int.Parse(Console.ReadLine()!)));
