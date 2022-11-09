9fd61218b16b33e426aeff9b566ae19449f5be2b
ValerValerich <zvalera1991@mail.ru>
2022-11-09 23:05:56 +0300

    Ex 30 Ok

---
+++ Ex_30/Program.cs
@@ -0,0 +1,19 @@
+﻿// 30. выводит массив из 8 элементов, заполненный нулями и единицами в рандоме
+
+Console.Clear();
+
+int[] GetArr(int x)
+{
+    int[] a = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        a[i] = new Random().Next(0, 2);
+    }
+    return a;
+}
+
+Console.WriteLine("Введите длину массива");
+foreach (int i in GetArr(int.Parse(Console.ReadLine()!)))
+{
+    Console.Write(i);
+}
