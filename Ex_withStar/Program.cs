ad5cffd4fecd17843a2d4671b8becc4d875c1e63
ValerValerich <zvalera1991@mail.ru>
2022-11-10 02:16:46 +0300

    Ex withStar Ok

---
+++ Ex_withStar/Program.cs
@@ -0,0 +1,27 @@
+﻿// Со звездочкой. Принимает число - высоту ёлочки и рисует ее в консоли звездочками
+
+Console.Clear();
+
+void PaintWood()
+{
+    int x = int.Parse(Console.ReadLine()!);
+    char[] a = new char[x * 2 - 1];
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = ' ';
+    }
+    for (int i = 0; a[0] != '*'; i++)
+    {
+
+        a[a.Length / 2 + i] = '*';
+        a[a.Length / 2 - i] = '*';
+        Console.WriteLine(String.Join("", a));
+    }
+}
+
+Console.WriteLine("Введите число - высоту ёлочки");
+PaintWood();
+
+
+
+
