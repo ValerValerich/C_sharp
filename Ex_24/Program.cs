deb8b2ad5689479d70a3445f551de3ad95da8543
ValerValerich <zvalera1991@mail.ru>
2022-11-09 22:09:34 +0300

    Ex 26 Ok

---
+++ Ex_24/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24. Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Summator(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Summator(n));
---
+++ Ex_26/Program.cs
@@ -0,0 +1,21 @@
+﻿// На выход число и выдает количество цифр в нём
+
+Console.Clear();
+
+int HowManyFigure(int x)
+{
+    int result = 0;
+    if (x != 0)
+    {
+        while (x != 0)
+        {
+            x /= 10;
+            result += 1;
+        }
+    }
+    else { result = 1; }
+    return result;
+}
+
+Console.WriteLine("Введите число для определения количества цифр в нем");
+Console.WriteLine(HowManyFigure(int.Parse(Console.ReadLine()!)));
