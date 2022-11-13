34bde49685e34d1eac0ccf6ae226d365b9f2c05e
ValerValerich <zvalera1991@mail.ru>
2022-11-13 22:35:56 +0300

    add Ex 34 Ok

---
+++ Ex_34/Program.cs
@@ -0,0 +1,26 @@
+﻿// 34. Задать массив рандомных положительных трехзначных чисел. Показать количество четных.
+
+Console.Clear();
+
+int[] GetArray(int size = 5, int minValue = 100, int maxValue = 999)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    Console.WriteLine(String.Join(',', res));
+    return res;
+}
+
+int cntPositiv(int[] arr)
+{
+    int cnt = 0;
+    foreach (int i in arr)
+    {
+        cnt+=i % 2 == 0 ? 1 : 0;
+    }
+    return cnt;
+}
+
+Console.WriteLine(cntPositiv(GetArray()));
