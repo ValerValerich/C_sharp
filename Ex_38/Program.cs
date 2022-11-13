dc731d5a81f340e758c6fa8e4fcb458ff931cc4b
ValerValerich <zvalera1991@mail.ru>
2022-11-13 23:25:58 +0300

    add Ex 38 Ok

---
+++ Ex_38/Program.cs
@@ -0,0 +1,28 @@
+﻿// 38. Задать массив вещественных чисел. Найти разницу между минимальным и максимальным
+
+Console.Clear();
+
+double[] GetArray(int size = 7, double minValue = 1.0, double maxValue = 10.0) // Указываю условия здесь, потому что надоело каждый раз вводить с консоли
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = Math.Round(new Random().NextDouble() * (maxValue), 3);
+    }
+    Console.WriteLine(String.Join(' ', res));
+    return res;
+}
+
+double findMinMaxDiff(double[] arr)
+{
+    double minVal = arr[0];
+    double maxVal = arr[0];
+    foreach (double el in arr)
+    {
+        minVal = el < minVal ? el : minVal;
+        maxVal = el > maxVal ? el : maxVal;
+    }
+    return Math.Round(maxVal - minVal, 3);
+}
+
+Console.WriteLine(findMinMaxDiff(GetArray()));
