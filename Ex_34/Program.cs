b078f72fe41d89c3de6f4317edcabce73e3f46d0
ValerValerich <zvalera1991@mail.ru>
2022-11-13 22:49:15 +0300

    add Ex 36 Ok

--- Ex_34/Program.cs
+++ Ex_34/Program.cs
@@ -2,7 +2,7 @@
 
 Console.Clear();
 
-int[] GetArray(int size = 5, int minValue = 100, int maxValue = 999)
+int[] GetArray(int size = 5, int minValue = 100, int maxValue = 999) // Указываю условия здесь, потому что надоело каждый раз вводить с консоли
 {
     int[] res = new int[size];
     for (int i = 0; i < size; i++)
@@ -18,7 +18,7 @@ int cntPositiv(int[] arr)
     int cnt = 0;
     foreach (int i in arr)
     {
-        cnt+=i % 2 == 0 ? 1 : 0;
+        cnt += i % 2 == 0 ? 1 : 0;
     }
     return cnt;
 }
---
+++ Ex_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// 36. Задать одномерный массив с рандомными числами. Найти сумму элементов, стоящих на нечетных позициях.
+
+Console.Clear();
+
+int[] GetArray(int size = 7, int minValue = 1, int maxValue = 10) // Указываю условия здесь, потому что надоело каждый раз вводить с консоли
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    Console.WriteLine(String.Join(',', res));
+    return res;
+}
+
+void sumOdd(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    Console.WriteLine(sum);
+}
+
+sumOdd(GetArray());
